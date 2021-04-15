af3cd918849975314efc2184eabeca7126a4c07a
Estuardo <2986622050101@ingenieria.usac.edu.gt>
2021-04-15 15:02:31 -0600

    Hoja de trabajo
    Hoja de trabajo

---
+++ HojaDeTrabajo.py
@@ -0,0 +1,7 @@
+masa=input("Cual es su peso en kg? ")
+
+altura=float(input("Cual es su altura? "))**2
+
+imc=round(float(masa)/float(altura),2)
+
+print("tu indice de masa coporal es: ", imc)
---
+++ clase2.py
@@ -0,0 +1,40 @@
+vgloval="mi Variable global"
+
+if 5>2:
+    print("Fice is greater than two!")
+
+if 5>2:
+    print("Five is greater than two!")
+    print("Hola mundo")
+
+mivariable=0
+x=5.1
+y="Hello, World!"
+x1=5
+y2="John"
+print(x1)
+print(y2)
+
+x=[0,"Hola Mundo"]
+print(x)
+
+#Comentario de una linea
+
+'''
+Comentario Multilinea
+'''
+
+x, y, z= "Hola", "Banana", "Cherry"
+
+x={"hola","adios"}
+
+x.add("Hola")
+
+print(x)
+
+print(x , y ,z)
+
+def myfunc():
+    print(vgloval)
+
+myfunc()
